9d94b0bd0f693f0e180161266e553176fb510934
Erik Verheul <erik@verheulconsultants.nl>
2015-09-20 12:00:57 +0200

    Merge with origin/master 1630
    Removed unused import.

--- test/src/test/java/hudson/slaves/CommandLauncherTest.java
+++ test/src/test/java/hudson/slaves/CommandLauncherTest.java
@@ -26,7 +26,6 @@ package hudson.slaves;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.not;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.junit.Assume.assumeTrue;
